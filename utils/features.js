e66a14c5048a6e6b1f22ae0b439daad1e5ef6cc0
Neel Darji <neeldarji089@gmail.com>
2024-05-13 22:46:45 +0530

    minor bug fixed

--- utils/features.js
+++ utils/features.js
@@ -13,7 +13,7 @@ const cookieOptions = {
 
 const connectDB = (uri) => {
   mongoose
-    .connect(uri, { dbName: "Chattu" }) //ChatApp
+    .connect(uri, { dbName: "ChatApp" })
     .then((data) => console.log(`Connected to DB:${data.connection.host}`))
     .catch((err) => {
       throw err;
